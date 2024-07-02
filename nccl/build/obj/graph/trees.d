658d993fb96bc0616389ea1f4705aaf0a5923b7c
Zhiyi Hu <zhu@daint104.login.cscs.ch>
2024-07-02 11:24:33 +0200

    Modify nccl 2.17 after applying npkit patch by adding npkit tracing code and changing INFO NCCL_GRAPH

---
+++ nccl/build/obj/graph/trees.d
@@ -0,0 +1,97 @@
+/users/zhu/nccl_npkit/nccl/build/obj/graph/trees.o: graph/trees.cc /usr/include/stdc-predef.h \
+ /users/zhu/nccl_npkit/nccl/build/include/nccl.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_runtime.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/crt/host_config.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/builtin_types.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/device_types.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/crt/host_defines.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/driver_types.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/vector_types.h \
+ /usr/lib64/gcc/x86_64-suse-linux/7/include-fixed/limits.h \
+ /usr/lib64/gcc/x86_64-suse-linux/7/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/posix1_lim.h /usr/include/bits/local_lim.h \
+ /usr/include/linux/limits.h /usr/include/bits/posix2_lim.h \
+ /usr/include/bits/xopen_lim.h /usr/include/bits/uio_lim.h \
+ /usr/lib64/gcc/x86_64-suse-linux/7/include/stddef.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/surface_types.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/texture_types.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/library_types.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/channel_descriptor.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_runtime_api.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_device_runtime_api.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/driver_functions.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/vector_functions.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/vector_functions.hpp \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_fp16.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_fp16.hpp \
+ /usr/include/c++/7/utility \
+ /usr/include/c++/7/x86_64-suse-linux/bits/c++config.h \
+ /usr/include/c++/7/x86_64-suse-linux/bits/os_defines.h \
+ /usr/include/c++/7/x86_64-suse-linux/bits/cpu_defines.h \
+ /usr/include/c++/7/bits/stl_relops.h /usr/include/c++/7/bits/stl_pair.h \
+ /usr/include/c++/7/bits/move.h /usr/include/c++/7/bits/concept_check.h \
+ /usr/include/c++/7/type_traits /usr/include/c++/7/initializer_list \
+ /usr/include/c++/7/cstring /usr/include/string.h \
+ /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
+ /usr/include/strings.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_bf16.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_bf16.hpp
+graph/trees.cc:
+/usr/include/stdc-predef.h:
+/users/zhu/nccl_npkit/nccl/build/include/nccl.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_runtime.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/crt/host_config.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/builtin_types.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/device_types.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/crt/host_defines.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/driver_types.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/vector_types.h:
+/usr/lib64/gcc/x86_64-suse-linux/7/include-fixed/limits.h:
+/usr/lib64/gcc/x86_64-suse-linux/7/include-fixed/syslimits.h:
+/usr/include/limits.h:
+/usr/include/bits/libc-header-start.h:
+/usr/include/features.h:
+/usr/include/sys/cdefs.h:
+/usr/include/bits/wordsize.h:
+/usr/include/bits/long-double.h:
+/usr/include/gnu/stubs.h:
+/usr/include/gnu/stubs-64.h:
+/usr/include/bits/posix1_lim.h:
+/usr/include/bits/local_lim.h:
+/usr/include/linux/limits.h:
+/usr/include/bits/posix2_lim.h:
+/usr/include/bits/xopen_lim.h:
+/usr/include/bits/uio_lim.h:
+/usr/lib64/gcc/x86_64-suse-linux/7/include/stddef.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/surface_types.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/texture_types.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/library_types.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/channel_descriptor.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_runtime_api.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_device_runtime_api.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/driver_functions.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/vector_functions.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/vector_functions.hpp:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_fp16.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_fp16.hpp:
+/usr/include/c++/7/utility:
+/usr/include/c++/7/x86_64-suse-linux/bits/c++config.h:
+/usr/include/c++/7/x86_64-suse-linux/bits/os_defines.h:
+/usr/include/c++/7/x86_64-suse-linux/bits/cpu_defines.h:
+/usr/include/c++/7/bits/stl_relops.h:
+/usr/include/c++/7/bits/stl_pair.h:
+/usr/include/c++/7/bits/move.h:
+/usr/include/c++/7/bits/concept_check.h:
+/usr/include/c++/7/type_traits:
+/usr/include/c++/7/initializer_list:
+/usr/include/c++/7/cstring:
+/usr/include/string.h:
+/usr/include/bits/types/locale_t.h:
+/usr/include/bits/types/__locale_t.h:
+/usr/include/strings.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_bf16.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_bf16.hpp:
---
+++ nccl/build/obj/misc/socket.d
@@ -0,0 +1,297 @@
+/users/zhu/nccl_npkit/nccl/build/obj/misc/socket.o: misc/socket.cc /usr/include/stdc-predef.h include/socket.h \
+ /users/zhu/nccl_npkit/nccl/build/include/nccl.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_runtime.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/crt/host_config.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/builtin_types.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/device_types.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/crt/host_defines.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/driver_types.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/vector_types.h \
+ /usr/lib64/gcc/x86_64-suse-linux/7/include-fixed/limits.h \
+ /usr/lib64/gcc/x86_64-suse-linux/7/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/posix1_lim.h /usr/include/bits/local_lim.h \
+ /usr/include/linux/limits.h /usr/include/bits/posix2_lim.h \
+ /usr/include/bits/xopen_lim.h /usr/include/bits/uio_lim.h \
+ /usr/lib64/gcc/x86_64-suse-linux/7/include/stddef.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/surface_types.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/texture_types.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/library_types.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/channel_descriptor.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_runtime_api.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_device_runtime_api.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/driver_functions.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/vector_functions.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/vector_functions.hpp \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_fp16.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_fp16.hpp \
+ /usr/include/c++/7/utility \
+ /usr/include/c++/7/x86_64-suse-linux/bits/c++config.h \
+ /usr/include/c++/7/x86_64-suse-linux/bits/os_defines.h \
+ /usr/include/c++/7/x86_64-suse-linux/bits/cpu_defines.h \
+ /usr/include/c++/7/bits/stl_relops.h /usr/include/c++/7/bits/stl_pair.h \
+ /usr/include/c++/7/bits/move.h /usr/include/c++/7/bits/concept_check.h \
+ /usr/include/c++/7/type_traits /usr/include/c++/7/initializer_list \
+ /usr/include/c++/7/cstring /usr/include/string.h \
+ /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
+ /usr/include/strings.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_bf16.h \
+ /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_bf16.hpp \
+ /usr/include/sys/socket.h /usr/include/bits/types/struct_iovec.h \
+ /usr/include/bits/socket.h /usr/include/sys/types.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/types/clock_t.h /usr/include/bits/types/clockid_t.h \
+ /usr/include/bits/types/time_t.h /usr/include/bits/types/timer_t.h \
+ /usr/include/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/bits/endian.h /usr/include/bits/byteswap.h \
+ /usr/include/bits/byteswap-16.h /usr/include/bits/uintn-identity.h \
+ /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/types/sigset_t.h /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/struct_timespec.h /usr/include/sys/sysmacros.h \
+ /usr/include/bits/sysmacros.h /usr/include/bits/pthreadtypes.h \
+ /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h /usr/include/bits/socket_type.h \
+ /usr/include/bits/sockaddr.h /usr/include/asm/socket.h \
+ /usr/include/asm-generic/socket.h /usr/include/linux/posix_types.h \
+ /usr/include/linux/stddef.h /usr/include/asm/posix_types.h \
+ /usr/include/asm/posix_types_64.h /usr/include/asm-generic/posix_types.h \
+ /usr/include/asm/bitsperlong.h /usr/include/asm-generic/bitsperlong.h \
+ /usr/include/asm/sockios.h /usr/include/asm-generic/sockios.h \
+ /usr/include/bits/types/struct_osockaddr.h /usr/include/arpa/inet.h \
+ /usr/include/netinet/in.h /usr/include/bits/stdint-uintn.h \
+ /usr/include/bits/in.h /usr/include/netinet/tcp.h \
+ /usr/lib64/gcc/x86_64-suse-linux/7/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/wchar.h /usr/include/netdb.h \
+ /usr/include/rpc/netdb.h /usr/include/bits/types/sigevent_t.h \
+ /usr/include/bits/types/sigval_t.h /usr/include/bits/netdb.h \
+ /usr/include/fcntl.h /usr/include/bits/fcntl.h \
+ /usr/include/bits/fcntl-linux.h /usr/include/linux/falloc.h \
+ /usr/include/bits/stat.h /usr/include/poll.h /usr/include/sys/poll.h \
+ /usr/include/bits/poll.h include/utils.h include/alloc.h \
+ include/checks.h include/debug.h include/nccl_net.h /usr/include/stdio.h \
+ /usr/include/bits/types/__FILE.h /usr/include/bits/types/FILE.h \
+ /usr/include/libio.h /usr/include/_G_config.h \
+ /usr/include/bits/types/__mbstate_t.h \
+ /usr/lib64/gcc/x86_64-suse-linux/7/include/stdarg.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ /usr/include/bits/stdio.h /usr/include/c++/7/chrono \
+ /usr/include/c++/7/ratio /usr/include/c++/7/cstdint \
+ /usr/include/c++/7/limits /usr/include/c++/7/ctime /usr/include/time.h \
+ /usr/include/bits/time.h /usr/include/bits/timex.h \
+ /usr/include/bits/types/struct_tm.h \
+ /usr/include/bits/types/struct_itimerspec.h \
+ /usr/include/c++/7/bits/parse_numbers.h /usr/include/pthread.h \
+ /usr/include/sched.h /usr/include/bits/sched.h \
+ /usr/include/bits/cpu-set.h /usr/include/bits/setjmp.h \
+ /usr/include/errno.h /usr/include/bits/errno.h \
+ /usr/include/linux/errno.h /usr/include/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ include/align.h include/utils.h /usr/include/sys/mman.h \
+ /usr/include/bits/mman.h /usr/include/bits/mman-linux.h \
+ /usr/include/unistd.h /usr/include/bits/posix_opt.h \
+ /usr/include/bits/environments.h /usr/include/bits/confname.h \
+ /usr/include/bits/getopt_posix.h /usr/include/bits/getopt_core.h \
+ /usr/include/c++/7/stdlib.h /usr/include/c++/7/cstdlib \
+ /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+ /usr/include/bits/waitstatus.h /usr/include/bits/floatn.h \
+ /usr/include/alloca.h /usr/include/bits/stdlib-bsearch.h \
+ /usr/include/bits/stdlib-float.h /usr/include/c++/7/bits/std_abs.h \
+ /usr/include/c++/7/new /usr/include/c++/7/exception \
+ /usr/include/c++/7/bits/exception.h \
+ /usr/include/c++/7/bits/exception_ptr.h \
+ /usr/include/c++/7/bits/exception_defines.h \
+ /usr/include/c++/7/bits/cxxabi_init_exception.h \
+ /usr/include/c++/7/typeinfo /usr/include/c++/7/bits/hash_bytes.h \
+ /usr/include/c++/7/bits/nested_exception.h /usr/include/ifaddrs.h \
+ /usr/include/net/if.h
+misc/socket.cc:
+/usr/include/stdc-predef.h:
+include/socket.h:
+/users/zhu/nccl_npkit/nccl/build/include/nccl.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_runtime.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/crt/host_config.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/builtin_types.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/device_types.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/crt/host_defines.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/driver_types.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/vector_types.h:
+/usr/lib64/gcc/x86_64-suse-linux/7/include-fixed/limits.h:
+/usr/lib64/gcc/x86_64-suse-linux/7/include-fixed/syslimits.h:
+/usr/include/limits.h:
+/usr/include/bits/libc-header-start.h:
+/usr/include/features.h:
+/usr/include/sys/cdefs.h:
+/usr/include/bits/wordsize.h:
+/usr/include/bits/long-double.h:
+/usr/include/gnu/stubs.h:
+/usr/include/gnu/stubs-64.h:
+/usr/include/bits/posix1_lim.h:
+/usr/include/bits/local_lim.h:
+/usr/include/linux/limits.h:
+/usr/include/bits/posix2_lim.h:
+/usr/include/bits/xopen_lim.h:
+/usr/include/bits/uio_lim.h:
+/usr/lib64/gcc/x86_64-suse-linux/7/include/stddef.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/surface_types.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/texture_types.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/library_types.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/channel_descriptor.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_runtime_api.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_device_runtime_api.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/driver_functions.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/vector_functions.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/vector_functions.hpp:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_fp16.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_fp16.hpp:
+/usr/include/c++/7/utility:
+/usr/include/c++/7/x86_64-suse-linux/bits/c++config.h:
+/usr/include/c++/7/x86_64-suse-linux/bits/os_defines.h:
+/usr/include/c++/7/x86_64-suse-linux/bits/cpu_defines.h:
+/usr/include/c++/7/bits/stl_relops.h:
+/usr/include/c++/7/bits/stl_pair.h:
+/usr/include/c++/7/bits/move.h:
+/usr/include/c++/7/bits/concept_check.h:
+/usr/include/c++/7/type_traits:
+/usr/include/c++/7/initializer_list:
+/usr/include/c++/7/cstring:
+/usr/include/string.h:
+/usr/include/bits/types/locale_t.h:
+/usr/include/bits/types/__locale_t.h:
+/usr/include/strings.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_bf16.h:
+/opt/nvidia/hpc_sdk/Linux_x86_64/21.5/cuda/11.3/include/cuda_bf16.hpp:
+/usr/include/sys/socket.h:
+/usr/include/bits/types/struct_iovec.h:
+/usr/include/bits/socket.h:
+/usr/include/sys/types.h:
+/usr/include/bits/types.h:
+/usr/include/bits/typesizes.h:
+/usr/include/bits/types/clock_t.h:
+/usr/include/bits/types/clockid_t.h:
+/usr/include/bits/types/time_t.h:
+/usr/include/bits/types/timer_t.h:
+/usr/include/bits/stdint-intn.h:
+/usr/include/endian.h:
+/usr/include/bits/endian.h:
+/usr/include/bits/byteswap.h:
+/usr/include/bits/byteswap-16.h:
+/usr/include/bits/uintn-identity.h:
+/usr/include/sys/select.h:
+/usr/include/bits/select.h:
+/usr/include/bits/types/sigset_t.h:
+/usr/include/bits/types/__sigset_t.h:
+/usr/include/bits/types/struct_timeval.h:
+/usr/include/bits/types/struct_timespec.h:
+/usr/include/sys/sysmacros.h:
+/usr/include/bits/sysmacros.h:
+/usr/include/bits/pthreadtypes.h:
+/usr/include/bits/thread-shared-types.h:
+/usr/include/bits/pthreadtypes-arch.h:
+/usr/include/bits/socket_type.h:
+/usr/include/bits/sockaddr.h:
+/usr/include/asm/socket.h:
+/usr/include/asm-generic/socket.h:
+/usr/include/linux/posix_types.h:
+/usr/include/linux/stddef.h:
+/usr/include/asm/posix_types.h:
+/usr/include/asm/posix_types_64.h:
+/usr/include/asm-generic/posix_types.h:
+/usr/include/asm/bitsperlong.h:
+/usr/include/asm-generic/bitsperlong.h:
+/usr/include/asm/sockios.h:
+/usr/include/asm-generic/sockios.h:
+/usr/include/bits/types/struct_osockaddr.h:
+/usr/include/arpa/inet.h:
+/usr/include/netinet/in.h:
+/usr/include/bits/stdint-uintn.h:
+/usr/include/bits/in.h:
+/usr/include/netinet/tcp.h:
+/usr/lib64/gcc/x86_64-suse-linux/7/include/stdint.h:
+/usr/include/stdint.h:
+/usr/include/bits/wchar.h:
+/usr/include/netdb.h:
+/usr/include/rpc/netdb.h:
+/usr/include/bits/types/sigevent_t.h:
+/usr/include/bits/types/sigval_t.h:
+/usr/include/bits/netdb.h:
+/usr/include/fcntl.h:
+/usr/include/bits/fcntl.h:
+/usr/include/bits/fcntl-linux.h:
+/usr/include/linux/falloc.h:
+/usr/include/bits/stat.h:
+/usr/include/poll.h:
+/usr/include/sys/poll.h:
+/usr/include/bits/poll.h:
+include/utils.h:
+include/alloc.h:
+include/checks.h:
+include/debug.h:
+include/nccl_net.h:
+/usr/include/stdio.h:
+/usr/include/bits/types/__FILE.h:
+/usr/include/bits/types/FILE.h:
+/usr/include/libio.h:
+/usr/include/_G_config.h:
+/usr/include/bits/types/__mbstate_t.h:
+/usr/lib64/gcc/x86_64-suse-linux/7/include/stdarg.h:
+/usr/include/bits/stdio_lim.h:
+/usr/include/bits/sys_errlist.h:
+/usr/include/bits/stdio.h:
+/usr/include/c++/7/chrono:
+/usr/include/c++/7/ratio:
+/usr/include/c++/7/cstdint:
+/usr/include/c++/7/limits:
+/usr/include/c++/7/ctime:
+/usr/include/time.h:
+/usr/include/bits/time.h:
+/usr/include/bits/timex.h:
+/usr/include/bits/types/struct_tm.h:
+/usr/include/bits/types/struct_itimerspec.h:
+/usr/include/c++/7/bits/parse_numbers.h:
+/usr/include/pthread.h:
+/usr/include/sched.h:
+/usr/include/bits/sched.h:
+/usr/include/bits/cpu-set.h:
+/usr/include/bits/setjmp.h:
+/usr/include/errno.h:
+/usr/include/bits/errno.h:
+/usr/include/linux/errno.h:
+/usr/include/asm/errno.h:
+/usr/include/asm-generic/errno.h:
+/usr/include/asm-generic/errno-base.h:
+include/align.h:
+include/utils.h:
+/usr/include/sys/mman.h:
+/usr/include/bits/mman.h:
+/usr/include/bits/mman-linux.h:
+/usr/include/unistd.h:
+/usr/include/bits/posix_opt.h:
+/usr/include/bits/environments.h:
+/usr/include/bits/confname.h:
+/usr/include/bits/getopt_posix.h:
+/usr/include/bits/getopt_core.h:
+/usr/include/c++/7/stdlib.h:
+/usr/include/c++/7/cstdlib:
+/usr/include/stdlib.h:
+/usr/include/bits/waitflags.h:
+/usr/include/bits/waitstatus.h:
+/usr/include/bits/floatn.h:
+/usr/include/alloca.h:
+/usr/include/bits/stdlib-bsearch.h:
+/usr/include/bits/stdlib-float.h:
+/usr/include/c++/7/bits/std_abs.h:
+/usr/include/c++/7/new:
+/usr/include/c++/7/exception:
+/usr/include/c++/7/bits/exception.h:
+/usr/include/c++/7/bits/exception_ptr.h:
+/usr/include/c++/7/bits/exception_defines.h:
+/usr/include/c++/7/bits/cxxabi_init_exception.h:
+/usr/include/c++/7/typeinfo:
+/usr/include/c++/7/bits/hash_bytes.h:
+/usr/include/c++/7/bits/nested_exception.h:
+/usr/include/ifaddrs.h:
+/usr/include/net/if.h:
